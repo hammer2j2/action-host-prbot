aa444f8f8682e734b10f804b2fd7740a2c6ef455
Clayton Han-Mitchell <hammer2j2@gmail.com>
2022-12-03 11:12:13 -0800

    added on pull_request events

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 /**
  * @param {import('probot').Probot} app
  */
+
 module.exports = (app) => {
   app.log("Yay! The app was loaded!");
+
+  app.on(
+    ["pull_request.opened", "pull_request.edited", "pull_request.reopened"],
+    async (context) => {
+  
+      if (context.payload.pull_request.title.indexOf('🤖') > -1) {
+        await context.octokit.pulls.createReview({
+          ...context.pullRequest(),
+          event: 'APPROVE'
+        })
+      }
+    }
+  );
 
   app.on("issues.opened", async (context) => {
     return context.octokit.issues.createComment(
